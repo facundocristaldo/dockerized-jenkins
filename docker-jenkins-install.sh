67e21e124c5785e15b75c23bf6f99ec57db94e19
facundocristaldo <40257119+facundocristaldo@users.noreply.github.com>
2021-04-08 20:31:25 -0300

    Update docker-jenkins-install.sh

--- docker-jenkins-install.sh
+++ docker-jenkins-install.sh
@@ -34,7 +34,7 @@ echo "Going to build jenkins-docker image"
 docker build -t jenkins-docker .
 
 echo "Going to run the docker image built"
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -d --name jenkins jenkins-docker
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/bin/docker-compose:/usr/local/bin/docker-compose -d --name jenkins jenkins-docker
 
 # show endpoint
 echo 'Jenkins installed'
